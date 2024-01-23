bfd6972e885e5d3910e2ba7520cbb95e21d1578f
Luke Noble <lukenoble@myop.ac.nz>
2024-01-23 13:19:47 +1300

    Removal of school column for users with only 1 school

--- resources/js/components/Restricted/Instruments/ListComponents/InstrumentsTable.vue
+++ resources/js/components/Restricted/Instruments/ListComponents/InstrumentsTable.vue
@@ -8,7 +8,7 @@
           <th @click="sortData('attributes.type')">Type:</th>
           <th @click="sortData('attributes.notes')" style="width: 50%;">Notes:</th>
           <th @click="sortData('attributes.state.description')">State:</th>
-          <th @click="sortData('school.name')">School:</th>
+          <th @click="sortData('school.name')" v-if="user.getSchools.length > 1">School:</th>
         </tr>
       </thead>
     </table>
@@ -22,7 +22,7 @@
           <td>{{ instrument.attributes.type }}</td>
           <td style="width: 50%;">{{instrument.attributes.notes}}</td>
           <td>{{instrument.attributes.state.description}}</td>
-          <td>{{instrument.school.name}}</td>
+          <td v-if="user.getSchools.length > 1">{{instrument.school.name}}</td>
         </tr>
       </tbody>
     </table>
@@ -38,11 +38,13 @@
 import { useRouter } from 'vue-router'
 import useSorter from '../../../../composables/useSorter'
 import { useInstrumentStore } from '../../../../stores/instruments'
+import { useUserStore } from '/resources/js/stores/user';
 
 
   const router = useRouter()
   const sorter = useSorter()
   const instrumentStore = useInstrumentStore()
+  const user = useUserStore()
 
   const props = defineProps({instruments: Array})
 
