b3ccc41a077c0b4318fc70f7b482425afba31c7d
Luke Noble <lukenoble@myop.ac.nz>
2024-02-19 14:13:21 +1300

    Try catch created for document download

--- resources/js/components/Layouts/MainLayout/Elements/DownloadDocument.vue
+++ resources/js/components/Layouts/MainLayout/Elements/DownloadDocument.vue
@@ -34,29 +34,35 @@
     methods: {
         async fetchDownloadUrl() {
             try {
-                this.loading = true
+                this.loading = true;
                 const response = await axiosClient.get(`document/download/${this.id}`, {
                     responseType: 'arraybuffer',
                 });
+
                 // Get the content-disposition header from the response
                 const contentDisposition = response.headers['content-disposition'];
-                // Extract the file name from the content-disposition header
-                // const match = contentDisposition.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/);
-                const match = contentDisposition.match(/filename[^;=\n]*=([^;\n]*)/);
-                let serverFileName = match ? match[1] : 'downloaded_file';
-                //   serverFileName = serverFileName.replace(/^[\d_]+/, '')
-                const blob = new Blob([response.data], { type: 'application/pdf' });
-                const downloadUrl = window.URL.createObjectURL(blob);
-                // Set both downloadUrl and downloadFileName
-                this.downloadFileName = serverFileName;
-                this.downloadUrl = downloadUrl;
-                this.loading = false
-            }
-            catch (error) {
+
+                if (contentDisposition) {
+                    // Extract the file name from the content-disposition header
+                    const match = contentDisposition.match(/filename[^;=\n]*=([^;\n]*)/);
+                    let serverFileName = match ? match[1] : 'downloaded_file';
+                    // serverFileName = serverFileName.replace(/^[\d_]+/, '');
+                    const blob = new Blob([response.data], { type: 'application/pdf' });
+                    const downloadUrl = window.URL.createObjectURL(blob);
+                    // Set both downloadUrl and downloadFileName
+                    this.downloadFileName = serverFileName;
+                    this.downloadUrl = downloadUrl;
+                } else {
+                    console.error('Content-Disposition header not found in the response.');
+                }
+
+                this.loading = false;
+            } catch (error) {
                 console.error('Error fetching download URL', error);
-                this.loading = false
+                this.loading = false;
             }
         },
+
     },
     components: { LoadingSpinner }
 };
