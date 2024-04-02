cfcc3b580a90a45511fd1c8d75fc84574f666b17
Luke Noble <lukenoble@myop.ac.nz>
2024-04-02 14:55:59 +1300

    School update testing

--- resources/js/views/Restricted/Settings/settingsRoutes.js
+++ resources/js/views/Restricted/Settings/settingsRoutes.js
@@ -5,7 +5,7 @@ import SchoolSetup from './SchoolSetup.vue'
 import SchoolDetails from './SchoolSetup/SchoolDetails.vue'
 
 const settingsRoutes = [
-  { path: 'settings', meta: {breadcrumb: 'Settings', section: 'Settings'}, children: [
+  { path: 'settings', meta: {title: 'Settings', breadcrumb: 'Settings', section: 'Settings'}, children: [
     { path: '', name: 'Settings', component: Settings, meta: {section: 'Settings', depth: 1}},
     { path: 'privacy', name: 'Privacy', component: Privacy, meta: {breadcrumb: ' / Privacy Policy', section: 'Settings', depth: 2}},
     { path: 'ip-policy', name: 'IPPolicy', component: IPPolicy, meta: {breadcrumb: ' / Intellectual Property Policy', section: 'Settings', depth: 2}},
