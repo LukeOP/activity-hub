172a619c11835b2d810369a613d822ac5422d660
Luke Noble <lukenoble@myop.ac.nz>
2024-04-10 16:22:30 +1200

    remove pending attendance from filter

--- resources/js/components/Layouts/MainLayout/FilterForms/AttendanceReviewForm.vue
+++ resources/js/components/Layouts/MainLayout/FilterForms/AttendanceReviewForm.vue
@@ -52,7 +52,7 @@ const sorter = useSorter()
 filter.setReturned({})
 const originalData = ref(filter.getData)
 
-const attendanceArray = ref(Array.from(new Set(originalData.value.map(d => d.attendance))))
+const attendanceArray = ref(Array.from(new Set(originalData.value.filter(d => d.attendance != 'pending').map(d => d.attendance))))
 
 const tutorArray = computed(()=>{
   return originalData.value.reduce((result, d) => {
@@ -91,6 +91,7 @@ const filtering = ref({
 
 function returnFiltered(){
   let filteredAttendance = originalData.value
+  filteredAttendance = filteredAttendance.filter(att => att.attendance != 'pending')
   filteredAttendance = filteredAttendance.filter(att => att.date >= filtering.value.start_date && att.date <= filtering.value.end_date)
   if(filtering.value.attendance != '') filteredAttendance = filteredAttendance.filter(att => att.attendance == filtering.value.attendance)
   if(filtering.value.tutor != '') filteredAttendance = filteredAttendance.filter(d => d.tutor.id == filtering.value.tutor)
