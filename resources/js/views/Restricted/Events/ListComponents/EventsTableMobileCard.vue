c8740f9957e95b3ebfb2b2afaa58215f481921f2
Luke Noble <lukenoble@myop.ac.nz>
2024-04-11 19:38:56 +1200

    EventsList table responsive

--- resources/js/views/Restricted/Events/ListComponents/EventsTableMobileCard.vue
+++ resources/js/views/Restricted/Events/ListComponents/EventsTableMobileCard.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="row" :class="{active: active}" @click="active = !active">
-    <div class="col col-10">
-      <p>{{event.attributes.name}}</p>
-      <p>{{event.attributes.description}}</p>
+    <div class="event-item" :class="{active: showDetails}">
+        <div class="event-visible">
+            <div class="event-details" @click="showDetails = !showDetails">
+                <div>{{event.attributes.name}}</div>
+                <div>{{event.attributes.description}}</div>
+            </div>
+            <i v-if="jobsOverdue()" class="fa-solid fa-triangle-exclamation fa-2x"
+            :style="{color: showDetails ? 'white' : ''}" :class="{userAlert: userJobsOverdue()}"></i>
+            <i class="fa-solid fa-magnifying-glass ms-2" 
+            :style="{color: showDetails ? 'white' : '', borderColor: showDetails ? 'white' : ''}" @click="EventDetails"></i>
+        </div>
+        <div class="event-hidden" v-if="showDetails" @click="showDetails = !showDetails">
+          <div v-if="user.getSchools.length > 1">{{ event.school.name }}</div>
+          <div>{{event.attributes.notes}}</div>
+        </div>
     </div>
-    <div class="col col-2">
-      <i class="fa-solid fa-magnifying-glass ms-2" :class="{active: active}" @click="EventDetails"></i>
-    </div>
-    <p v-if="active && user.getSchools.length > 1">{{event.school.name}}</p>
-    <p v-if="active">{{event.attributes.notes}}</p>
-  </div>
 </template>
 
 <script setup>
@@ -17,16 +22,26 @@ import { useUserStore } from "/resources/js/stores/user";
 import { useEventStore } from "/resources/js/stores/events"
 import { ref } from "vue"
 import { useRouter } from "vue-router"
+import moment from "moment";
 const props = defineProps({event:Object})
 
 const router = useRouter()
 const eventStore = useEventStore()
 const user = useUserStore()
 
-const active = ref(false)
+const showDetails = ref(false)
+
+function jobsOverdue() {
+  return props.event.jobs.some(j => (j.due_date < moment().format('YYYY-MM-DD') && j.status == 0))
+}
+function userJobsOverdue(event){
+  if(user.hasPermission('EVENTS_E', props.event.school.id)) return true
+  let overdueJobs = props.event.jobs.filter(j => (j.due_date < moment().format('YYYY-MM-DD') && j.status == 0))
+  return overdueJobs.some(j => j.users.some(u => u.attributes.id == user.attributes.id))
+}
 
 function EventDetails(){
-  eventStore.setEvent(props.event)
+  eventStore.setEvent(props.event.id)
   router.push({
     name: 'EventDetails'
   })
@@ -36,27 +51,43 @@ function EventDetails(){
 
 <style lang="scss" scoped>
 
-.fa-magnifying-glass {
-  color: $ah-primary;
-  padding: 5px;
-  border: 1px solid $ah-primary;
-  border-radius: 6px;
-  margin-top: 7px;
-}
-.row {
-  padding: 10px;
-}
-.active {
-  background-color: $ah-primary-light;
-  color: lighten($ah-primary-light, 50%);
-  border-bottom: 1px solid $ah-grey;
-  .fa-magnifying-glass {
-    border-color: lighten($ah-primary-light, 50%);
+
+.event-item {
+  .event-visible {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    .fa-magnifying-glass {
+      color: $ah-primary;
+      padding: 5px;
+      border: 1px solid $ah-primary;
+      border-radius: 6px;
+    }
+    .event-details {
+      width: 100%;
+    }
+    .event-icon {
+        width: 45px;
+    }
+  }
+  .event-hidden {
+      padding: 10px;
+  }
+  &:nth-child(odd){
+      background-color: $ah-primary-background;
   }
 }
-p {
-  margin: 0;
+.active {
+    background-color: $ah-primary-light !important;
+    color: white;
+    border-bottom: 1px solid $ah-grey;
 }
-
+.fa-triangle-exclamation {
+  color: $ah-primary;
+}
+.userAlert{
+  color: rgb(222, 14, 14);
+}
 
 </style>
