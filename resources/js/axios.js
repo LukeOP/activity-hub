ddfa3ddf7895300bc87b07548b46a43f8ca7be7a
Luke Noble <76924915+LukeOP@users.noreply.github.com>
2024-04-21 12:36:46 +1200

    Update axios.js

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -8,7 +8,7 @@ else if(import.meta.env.VITE_ENV === 'development') url = 'http://dev.activityhu
 else if(import.meta.env.VITE_ENV === 'local') url = 'http://localhost:8000'
 else url = 'http://localhost:8000'
 
-console.log('BASE URL: ', url, 'ENV: ', import.meta.env.VITE_URL);
+console.log('BASE URL: ', url, 'ENV: ', import.meta.env.VITE_ENV);
 
 const axiosClient = axios.create({
   baseURL: url + '/api',
